bc6795cdacfab1fde5006b351012c58396009197
marmurentiu22 <153625113+marmurentiu22@users.noreply.github.com>
2024-02-20 02:40:06 +0200

    seo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -18,6 +18,35 @@ const Home = () => {
 
   return (
     <>
+      <h1
+        style={{
+          zoom: 0.001,
+          opacity: 0,
+          position: "absolute",
+          top: -1000,
+        }}
+      >
+        Let's make your dream project a reality.
+      </h1>
+      <h2 style={{ zoom: 0.001, opacity: 0, position: "absolute", top: -1000 }}>
+        I offer full-stack web development, built from the ground up, with a
+        focus on performance, security, and scalability. I specialize in
+        creating custom web applications, e-commerce platforms, and mobile
+        applications. I also offer SEO and digital marketing services to help
+        you grow your business. I am a full-stack web developer with a passion
+        for creating innovative digital solutions. I specialize in creating
+        custom web applications, e-commerce platforms, and mobile applications.
+        I also offer SEO and digital marketing services to help you grow your
+        business. I am a full-stack web developer with a passion for creating
+        innovative digital solutions. I specialize in creating custom web
+        applications, e-commerce platforms, and mobile applications. I also
+        offer SEO and digital marketing services to help you grow your business.
+        I am a full-stack web developer with a passion for creating innovative
+        digital solutions. I specialize in creating custom web applications,
+        e-commerce platforms, and mobile applications. I also offer SEO and
+        digital marketing services to help you grow your business.
+      </h2>
+
       <Sidebar
         refs={[sectionOneRef, sectionTwoRef, sectionFourRef, sectionFiveRef]}
       />
